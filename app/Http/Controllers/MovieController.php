93776db6769d3a33c7d0a6108f25f4f8da425d89
Andor <andorhorvat@gmail.com>
2020-03-20 20:59:38 +0100

    Zadatak 2

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Movie;
+use Illuminate\Http\JsonResponse;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Movie::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $movie = new Movie();
+        $movie->title = $request->title;
+        $movie->director = $request->director;
+        $movie->imageUrl = $request->imageUrl;
+        $movie->duration = $request->duration;
+        $movie->releaseDate = $request->releaseDate;
+        $movie->genre = $request->genre;
+        $movie->save();
+        return $movie;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Movie::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $movie = Movie::findOrFail($id);
+        $movie->title = $request->title;
+        $movie->director = $request->director;
+        $movie->imageUrl = $request->imageUrl;
+        $movie->duration = $request->duration;
+        $movie->releaseDate = $request->releaseDate;
+        $movie->genre = $request->genre;
+        $movie->save();
+        return $movie;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $movie = Movie::find($id);
+        $movie->delete();
+        return new JsonResponse(true);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('movies','MovieController');
+
+/*
+D:\API\movies-api>php artisan route:list
++--------+-----------+-------------------------+----------------+----------------------------------------------+--------------+
+| Domain | Method    | URI                     | Name           | Action                                       | Middleware   |
++--------+-----------+-------------------------+----------------+----------------------------------------------+--------------+
+|        | GET|HEAD  | /                       |                | Closure                                      | web          |
+|        | GET|HEAD  | api/movies              | movies.index   | App\Http\Controllers\MovieController@index   | api          |
+|        | POST      | api/movies              | movies.store   | App\Http\Controllers\MovieController@store   | api          |
+|        | GET|HEAD  | api/movies/create       | movies.create  | App\Http\Controllers\MovieController@create  | api          |
+|        | GET|HEAD  | api/movies/{movie}      | movies.show    | App\Http\Controllers\MovieController@show    | api          |
+|        | PUT|PATCH | api/movies/{movie}      | movies.update  | App\Http\Controllers\MovieController@update  | api          |
+|        | DELETE    | api/movies/{movie}      | movies.destroy | App\Http\Controllers\MovieController@destroy | api          |
+|        | GET|HEAD  | api/movies/{movie}/edit | movies.edit    | App\Http\Controllers\MovieController@edit    | api          |
+|        | GET|HEAD  | api/user                |                | Closure                                      | api,auth:api |
++--------+-----------+-------------------------+----------------+----------------------------------------------+--------------+
+*/
